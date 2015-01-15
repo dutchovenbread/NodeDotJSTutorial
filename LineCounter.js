989d26cf004d76d6b3660f022800fd6a512693c5
Michael Hunter <hunter.michael.k@gmail.com>
2015-01-15 14:01:54 -0500

    Completed third exercise.

--- LineCounter.js
+++ LineCounter.js
@@ -1,4 +1,12 @@
 var fs = require('fs')
 
-path = process.argv[2]
+var path = process.argv[2]
+
+var buffer = fs.readFileSync(path)
+
+var string = buffer.toString()
+
+var count = string.split('\n').length  -1 //subtract one because the number of elements in the array is one less than the number of lines.
+
+console.log(count)
 
