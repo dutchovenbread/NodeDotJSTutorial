25913ac4b144015766b6638de082e3e5d9326341
Michael Hunter <hunter.michael.k@gmail.com>
2015-01-16 17:10:35 -0500

    Adjusted method name to something more appropriate.

--- LineCounterAsync.js
+++ LineCounterAsync.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 var path = process.argv[2]
 
-function readFile(){
+function countLines(){
   fs.readFile(path, function doneReading(err, buffer){
     var string = buffer.toString()
     var count = string.split('\n').length  -1 //subtract one because the number of elements in the array is one less than the number of lines.
@@ -9,4 +9,4 @@ function readFile(){
   })
 }
 
-readFile()
+countLines()
